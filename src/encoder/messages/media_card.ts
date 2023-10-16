37beec5a6507d4cdff4e9ad34f5626817944ee46
BSTluo <1946831552@qq.com>
2023-10-16 10:23:19 +0800

    modified:   src/encoder/messages/media_card.ts

--- src/encoder/messages/media_card.ts
+++ src/encoder/messages/media_card.ts
@@ -1,10 +1,17 @@
 import PublicMessage from './PublicMessage'
 
-export default (type: 'music' | 'video', title: string, singer: string, cover: string, BitRate: number, color: string) => {
+export default (type: 'music' | 'video', title: string, singer: string, cover: string, color: string, BitRate?: number) => {
   const typeMap = {
     music: 0,
     video: 1,
   }
-  const data = `m__4=${typeMap[type]}>${title}>${singer}>${cover}>${color}>${BitRate}`
+  let data
+
+  if (!BitRate) {
+    data = `m__4=${typeMap[type]}>${title}>${singer}>${cover}>${color}>11451`
+  } else {
+    data = `m__4=${typeMap[type]}>${title}>${singer}>${cover}>${color}>${BitRate}`
+  }
+
   return PublicMessage(data, color)
 }
