580a9f765df53cc8a657e13f2a394e21e9c0f53a
ajchen02 <88434425+ajchen02@users.noreply.github.com>
2025-07-20 20:51:27 +0800

    fix iirose cache

--- src/uploads.ts
+++ src/uploads.ts
@@ -4,12 +4,24 @@ import FormData from 'form-data';
 import { ReadStream } from 'fs';
 import axios from "axios";
 
+function formatCurrentTime() {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0');
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+}
+
 export async function upload(http: HTTP, url: string, file: Buffer, config: IIROSE_Bot.Config)
 {
 
   const formData = new FormData();
   
-  formData.append('source', file, "example.jpeg");
+  formData.append('source', file, `${formatCurrentTime()}.jpeg`);
   formData.append('expiration', `PT${config.picKill}M`);
   formData.append('key', config.picToken);
 
