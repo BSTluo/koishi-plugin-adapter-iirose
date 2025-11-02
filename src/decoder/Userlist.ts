fa281b8bdec8dd0d1429e6c3e5d824f78cedf17b
shangxueink <1919892171@qq.com>
2025-11-02 17:32:09 +0800

    解析大包

--- src/decoder/Userlist.ts
+++ src/decoder/Userlist.ts
@@ -24,63 +24,66 @@ export const userList = async (message: string, bot: IIROSE_Bot) =>
 {
   if (message.startsWith('%*"'))
   {
-    // 恢复调试日志输出
     console.log("========================================");
     console.log(message);
     console.log("========================================");
 
     const data = message.substring(3);
     const userList: UserList[] = [];
-    const roomIds = new Set<string>();
 
-    // 1. 解析用户列表并收集 Room ID
+    // 1. 解析用户列表
     data.split('<').forEach((e) =>
     {
       const tmp = e.split('>');
       if (tmp.length >= 9)
       {
-        const room = tmp[4];
-        if (room) roomIds.add(room);
         userList.push({
           avatar: tmp[0].startsWith('http') ? tmp[0] : `https://s.iirose.com/images/icon/${tmp[0]}.jpg`,
           username: h.unescape(tmp[2]),
           color: tmp[3],
-          room: room,
+          room: tmp[4],
           uid: tmp[8],
         });
       }
     });
 
     // 2. 解析房间信息
-    const roomList: Record<string, RoomInfo> = {};
-    const roomDataSegments = data.split('<');
+    const roomList = {};
+    const segments = data.split('<');
+    // 匹配一个或多个由下划线连接的、长度至少为10的十六进制字符串
+    const roomIdRegex = /^([a-f0-9]{10,}_?)+$/;
 
-    for (const roomId of roomIds)
+    for (let i = 1; i < segments.length; i++)
     {
-      // 查找包含房间ID的特定段
-      // 格式为 <roomId_... 或 <roomId>...
-      const roomSegment = roomDataSegments.find(s => s.startsWith(roomId + '_') || s.startsWith(roomId + '>'));
+      const currentSegment = segments[i];
+      const candidateId = currentSegment.split('>')[0];
 
-      if (roomSegment)
+      if (roomIdRegex.test(candidateId))
       {
-        // 房间数据在 <roomId... 之前的部分
-        const roomDataString = data.substring(0, data.indexOf('<' + roomSegment));
-        const lastPart = roomDataString.split('<').pop();
+        const roomDataFields = currentSegment.split('>');
+        const idPath = candidateId.split('_');
+        const roomName = roomDataFields[1] || '';
+        const description = roomDataFields[6] || ''; // 根据固定顺序，简介在第7个字段
 
-        if (lastPart)
+        let currentLevel = roomList;
+        for (let j = 0; j < idPath.length - 1; j++)
         {
-          const fields = lastPart.split('>');
-          // 倒数第二个字段是简介
-          const description = fields[fields.length - 2] || '';
-
-          roomList[roomId] = {
-            id: roomId,
-            name: roomSegment.split('>')[1] || '',
-            online: 0, // 无法从此结构中直接获取，暂定为0
-            description: description,
-            users: userList.filter(u => u.room === roomId).map(u => u.uid),
-          };
+          const idPart = idPath[j];
+          if (!currentLevel[idPart])
+          {
+            currentLevel[idPart] = {};
+          }
+          currentLevel = currentLevel[idPart];
         }
+
+        const finalId = idPath[idPath.length - 1];
+        currentLevel[finalId] = {
+          id: finalId,
+          name: roomName,
+          description: description,
+          users: userList.filter(u => u.room === finalId).map(u => u.uid),
+          online: userList.filter(u => u.room === finalId).length,
+        };
       }
     }
 
